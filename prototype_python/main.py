a259ab2bfd085fe6c06b4b19df50978107761c50
Osmane-cpp <63423818+Osmane-EL-MONTASER@users.noreply.github.com>
2023-02-16 21:41:37 +0100

    Update main.py

--- prototype_python/main.py
+++ prototype_python/main.py
@@ -5,7 +5,6 @@ app = Flask(__name__, static_url_path='/static/')
 # Définit une route pour la page d'accueil
 @app.route('/')
 def index():
-    # Définit une variable user
     return render_template('index.html')
 
 if __name__ == '__main__':
